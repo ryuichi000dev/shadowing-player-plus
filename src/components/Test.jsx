63a938a5f63d5f1205c006579f3e146bbac5e855
ryuichi000dev <ryuichi000dev@gmail.com>
2023-01-25 02:48:59 +0900

    ios test

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -272,7 +272,7 @@ export default function MusicPlayerSlider() {
 
   const recording = async () => {
     setLastPlayStatus("Recording");
-    await sleep(300);
+    await sleep(200);
     SE1Ref.current.play();
     await sleep(1000);
     recorderRef.current.start();
@@ -282,7 +282,7 @@ export default function MusicPlayerSlider() {
 
   const feedbacking = async () => {
     setLastPlayStatus("Feedbacking");
-    await sleep(300);
+    await sleep(200);
     SE2Ref.current.play();
     await sleep(1000);
     const recAudio = document.querySelector("#recAudio");
